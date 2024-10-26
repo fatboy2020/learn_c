a0b85f781676198221e4f091ad440e2256462cec
fatboy2020 <bailinlin2019@outlook.com>
2024-10-26 16:14:37 +0800

    pta1

---
+++ pta1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 打印第一行的星号
+    for (int i = 0; i < 12; i++) {
+        printf("*");
+    }
+    printf("\n");  // 换行
+
+    // 打印 "Welcome"
+    printf("  Welcome \n");
+
+    // 打印最后一行的星号
+    for (int i = 0; i < 12; i++) {
+        printf("*");
+    }
+   
+
+    return 0;
+}
